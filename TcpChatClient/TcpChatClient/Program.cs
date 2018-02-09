ea14b7a1ff5c52df9a6779a0816bf4da84849faf
SabreUrn <kasp5953@edu.easj.dk>
2018-02-09 03:38:02 +0100

    adds client project

---
+++ TcpChatClient/TcpChatClient/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TcpChatClient {
+    class Program {
+        static void Main(string[] args) {
+            //set up basics
+            //Run()
+        }
+
+        static void Run() {
+            //Connect()
+        }
+
+        //ReadMessage()
+        //WriteMessage()
+
+        //Connect()
+        //WaitForServer()
+    }
+}
