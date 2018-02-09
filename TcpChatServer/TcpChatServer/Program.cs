dfd78e924c48f52d47837742e0ddff3d018df900
SabreUrn <kasp5953@edu.easj.dk>
2018-02-09 16:34:22 +0100

    adds DoClient() for client creation and runs it per client

--- TcpChatServer/TcpChatServer/Program.cs
+++ TcpChatServer/TcpChatServer/Program.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TcpChatServer {
@@ -9,7 +13,14 @@ namespace TcpChatServer {
         static void Main(string[] args) {
 
             //set up basics (NetworkStream?)
-            //Run()
+            TcpListener serverSocket = new TcpListener(IPAddress.Any, 6789);
+            TcpClient connectionSocket;
+            serverSocket.Start();
+
+            while(true) {
+                connectionSocket = serverSocket.AcceptTcpClient();
+                ThreadPool.QueueUserWorkItem(DoClient, connectionSocket);
+            }
         }
 
         static void Run() {
@@ -26,5 +37,12 @@ namespace TcpChatServer {
             //client class?
             //add number to client for identification (property?)
             //sign-up where client is asked for own ID?
+        private static void DoClient(object obj) {
+            TcpClient client = (TcpClient)obj;
+            NetworkStream ns = client.GetStream();
+            StreamWriter sw = new StreamWriter(ns);
+            sw.AutoFlush = true;
+            StreamReader sr = new StreamReader(ns);
+        }
     }
 }
