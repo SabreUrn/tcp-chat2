f348844e2876355501ad20f55c99e08f906af9e9
SabreUrn <kasp5953@edu.easj.dk>
2018-02-09 03:34:23 +0100

    adds server project

---
+++ TcpChatServer/TcpChatServer/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TcpChatServer {
+    class Program {
+        static void Main(string[] args) {
+
+            //set up basics (NetworkStream?)
+            //Run()
+        }
+
+        static void Run() {
+            //listen for clients
+            //DoClient() where client properties are initialised
+            //
+        }
+
+
+        //ReadMessage()
+        //WriteMessage()
+
+        //DoClient()
+    }
+}
